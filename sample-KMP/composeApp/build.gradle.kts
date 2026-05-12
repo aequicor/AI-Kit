284cc44f45445e5c0b29b3eaeab09f9f5146c4da
KYamshanov <kyamshanov@vk.com>
2026-05-12 13:30:47 +0400

    kit: step 3/7 — deps-koin-kermit

--- sample-KMP/composeApp/build.gradle.kts
+++ sample-KMP/composeApp/build.gradle.kts
@@ -43,6 +43,9 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.shared)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.kermit)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- sample-KMP/shared/build.gradle.kts
+++ sample-KMP/shared/build.gradle.kts
@@ -19,7 +19,8 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation(libs.koin.core)
+            implementation(libs.kermit)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
